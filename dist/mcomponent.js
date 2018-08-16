43f04ad1cc38b404fa082b0a014ff012ef4a24f5
Packianathan <rajarajan.packianathan@siemens-healthineers.com>
2018-08-16 14:59:15 +0530

    linked MComponent to View class - see sample code in MComponent.js

--- dist/mcomponent.js
+++ dist/mcomponent.js
@@ -435,20 +435,24 @@ var MComponent = function () {
     var MComponent = function () {
         function MComponent() {
             classCallCheck(this, MComponent);
-
-            this._viewRef = new View(this.template, this, componentDOMRef);
         }
 
         createClass(MComponent, [{
-            key: "initView",
-            value: function initView() {
+            key: "_initView",
+            value: function _initView() {
                 this._viewRef.parse();
             }
         }, {
-            key: "init",
-            value: function init() {
-                this._viewRef.init();
+            key: "_init",
+            value: function _init(componentDOMRef) {
+                this._viewRef = new View(this.template, this, componentDOMRef);
                 this.mcOnInit();
+                this._initView();
+            }
+        }, {
+            key: "_initialRender",
+            value: function _initialRender() {
+                this._viewRef.initialRender();
             }
         }, {
             key: "mcOnInit",
@@ -462,6 +466,10 @@ var MComponent = function () {
         elem = document.querySelector(selector) || _throwError("Error at bootstrap, selector : " + selector + " doesn't exist in DOM");
         rootComponent = rootComponent || _throwError("rootComponent not found");
     };
+    MComponent.extend = function (childClass) {
+        childClass.prototype = Object.create(MComponent.prototype);
+        childClass.prototype.super = MComponent;
+    };
 
     function _throwError(msg) {
         throw new Error(msg);
@@ -470,4 +478,42 @@ var MComponent = function () {
     return MComponent;
 }();
 
-export { MComponent };
+/* Test sample code
+
+document.querySelector('body').innerHTML = "<div id='myObj'></div>";
+var sampletemplate = `<div class="container"><span>Your Name is: {{name }} {{lastName}} and your age is {{age}}</span>
+                        <div>
+                            <span>{{name}}'s Department: 
+                                <b>{{dept}}</b> 
+                             </span>
+                        </div>
+                        <div>age > 30 : {{age > 30}}</div>
+                        <div> Last name is Jones ? -- {{lastName === 'Jones'}}</div>
+                        <div> Your alias is different from your first name ? -- {{alias !== name || (alias === lastName) }}</div>
+                      </div>`;
+
+
+function mycomp (template) {
+    
+    this.super.call(this);
+    this.template = template;
+}
+
+MComponent.extend(mycomp);
+
+mycomp.prototype.mcOnInit = function(){
+    this.name = 'Steve',
+    this.lastName = "Smith";
+    this.age = 32;
+    this.dept = 'sales';
+    this.alias =  "Steve";
+}
+
+var myObj = new mycomp(sampletemplate);
+
+myObj._init(
+    document.getElementById('myObj')
+);
+myObj._initialRender();
+
+*/
--- src/component.js
+++ src/component.js
@@ -1,17 +1,19 @@
 import { View } from "./view";
 
-export var MComponent = (function() {
+var MComponent = (function() {
     class MComponent {
-        constructor() {
-            this._viewRef = new View(this.template, this, componentDOMRef);
-        }
-        initView() {
+        constructor() {}
+        _initView() {
             this._viewRef.parse();
         }
 
-        init() {
-            this._viewRef.init();
+        _init(componentDOMRef) {
+            this._viewRef = new View(this.template, this, componentDOMRef);
             this.mcOnInit();
+            this._initView();
+        }
+        _initialRender() {
+            this._viewRef.initialRender();
         }
 
         mcOnInit() {}
@@ -27,6 +29,10 @@ export var MComponent = (function() {
             );
         rootComponent = rootComponent || _throwError("rootComponent not found");
     };
+    MComponent.extend = function(childClass) {
+        childClass.prototype = Object.create(MComponent.prototype);
+        childClass.prototype.super = MComponent;
+    };
 
     function _throwError(msg) {
         throw new Error(msg);
@@ -34,3 +40,43 @@ export var MComponent = (function() {
 
     return MComponent;
 })();
+
+/* Test sample code
+
+document.querySelector('body').innerHTML = "<div id='myObj'></div>";
+var sampletemplate = `<div class="container"><span>Your Name is: {{name }} {{lastName}} and your age is {{age}}</span>
+                        <div>
+                            <span>{{name}}'s Department: 
+                                <b>{{dept}}</b> 
+                             </span>
+                        </div>
+                        <div>age > 30 : {{age > 30}}</div>
+                        <div> Last name is Jones ? -- {{lastName === 'Jones'}}</div>
+                        <div> Your alias is different from your first name ? -- {{alias !== name || (alias === lastName) }}</div>
+                      </div>`;
+
+
+function mycomp (template) {
+    
+    this.super.call(this);
+    this.template = template;
+}
+
+MComponent.extend(mycomp);
+
+mycomp.prototype.mcOnInit = function(){
+    this.name = 'Steve',
+    this.lastName = "Smith";
+    this.age = 32;
+    this.dept = 'sales';
+    this.alias =  "Steve";
+}
+
+var myObj = new mycomp(sampletemplate);
+
+myObj._init(
+    document.getElementById('myObj')
+);
+myObj._initialRender();
+
+*/
